becf3f13309e9d2795077decda438c42e2f928ac
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-08-18 19:27:26 -0300

    Exercicio Variaveis
    Exercicio Variaveis

---
+++ Exercicios/Variaveis/Variaveis.js
@@ -0,0 +1,10 @@
+//let idade = 5;
+//console.log(idade);
+//let Altura = 180;
+//console.log(Altura);
+let nome = "Joao Ricardo";
+console.log(nome);
+let sobrenome = "Soares";
+console.log(sobrenome);
+let cidade = "niteroi";
+console.log(cidade);
