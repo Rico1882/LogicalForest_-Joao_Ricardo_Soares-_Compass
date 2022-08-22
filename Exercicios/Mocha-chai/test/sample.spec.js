367d81cf713d859fd7d2451083b774ee19f7fc87
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-08-22 18:46:58 -0300

    Atualização Mocha

--- Exercicios/Mocha-chai/test/sample.spec.js
+++ Exercicios/Mocha-chai/test/sample.spec.js
@@ -1,8 +1,24 @@
+import assert from "assert"
 
-describe('', () => {
-
+describe('Meu primeiro teste', () => {
+    it('Verificar uma igualdade', () => {
+        let aux = 2
+        assert.strictEqual(aux, 2)
+})
+    it('Verificar outra igualdade', () => {
+        let aux = 'carro'
+        assert.strictEqual(aux, 'carro')
 })
 
-it('', () => {
-    
+describe('Meu segundo teste', () => {
+    it('Verificar uma desigualdade', () => {
+        let aux = 2
+        assert.notStrictEqual(aux, '2')
+})
+    it('Verificar outra desigualdade', () => {
+        let aux = 'carro'
+        assert.notStrictEqual(aux, 'charrete')
+})
+})
+
 })
