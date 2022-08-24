169c2b35bba4dabd8711c71e29fbf9e86e4bfb0c
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-08-24 17:51:48 -0300

    Atualização

--- Projeto Calculadora/src/Calculadora.js
+++ Projeto Calculadora/src/Calculadora.js
@@ -3,19 +3,18 @@ export default class Calculadora {
 
     static soma (a, b) {
         return a + b
-
     }
+    
     static sub (a, b) {
         return a - b
-
     }
+    
     static mult (a, b) {
         return a * b
-
     }
+    
     static div (a, b) {
         return a / b
-
     }
 
     static raiz (a) {
@@ -29,7 +28,7 @@ export default class Calculadora {
     static raiz_cub (a) {
         return Math.cbrt (a)
     }
-
+    
     static potencia (a, b) {
         return Math.pow (a, b)
     }
